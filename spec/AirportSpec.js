0d1650db8b895034ab6d591807d1dd47de0632ea
Natalie <49326857+natyeo@users.noreply.github.com>
2019-11-12 17:07:28 +0000

    landing when airport full throws error

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -7,9 +7,18 @@ describe('Airport', function() {
     plane = jasmine.createSpy();
   });
 
-  it('can land a plane', function() {
-    expect(airport.land(plane)).toEqual(plane);
-  });
+  describe('landing', function() {
+    it('can land a plane', function() {
+      expect(airport.land(plane)).toEqual(plane);
+    });
+
+    it('cannot land when airport is full', function() {
+      airport.land(plane)
+      expect(function() {
+        airport.land(plane);
+      }).toThrow("airport full!")
+    });
+  })
 
   describe('taking off', function() {
     beforeEach(function() {
--- src/Airport.js
+++ src/Airport.js
@@ -1,16 +1,16 @@
 function Airport() {
   this._planes = [];
+  this._capacity = 1;
 };
 
 Airport.prototype.land = function(plane) {
+  if (this._planes.length >= this._capacity) throw "airport full!";
   this._planes.push(plane);
   return plane;
 };
 
 Airport.prototype.takeOff = function(plane) {
-  console.log('planes before: ' + this._planes);
   index = this._planes.indexOf(plane);
   this._planes.splice(index, 1);
-  console.log('planes after: ' + this._planes);
   return plane;
 }
